eb8939c4ddc1d5c6820fae082da9bef90eacc79d
Surya Nitish <suryasnitish@gmail.com>
2017-10-14 12:52:47 +0530

    Commiting Spring

--- src/com/pantrycup/ajax/ServiceProviderAjaxListener.java
+++ src/com/pantrycup/ajax/ServiceProviderAjaxListener.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServiceProviderAjaxListener
 {
-	@ResponseBody
-	@RequestMapping(value = "/search/allproviders")
-	public AjaxResponseBody getSearchResultViaAjax() {
+//	@ResponseBody
+//	@RequestMapping(value = "/search/allproviders")
+	//public AjaxResponseBody getSearchResultViaAjax() {
 
-		AjaxResponseBody result = new AjaxResponseBody();
+	//	AjaxResponseBody result = new AjaxResponseBody();
 		//logic
-		return result;
+	//	return result;
 	}
-}
+
