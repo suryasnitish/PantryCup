8603972bcfb5639ed14d76eec88cb63200aa1fec
Surya Nitish <suryasnitish@gmail.com>
2017-10-14 13:11:30 +0530

    Commiting AJAX Start

--- src/com/pantrycup/controllers/ServiceProviderAjaxService.java
+++ src/com/pantrycup/controllers/ServiceProviderAjaxService.java
@@ -1,7 +1,13 @@
 package com.pantrycup.controllers;
 
+import java.util.List;
+
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pantrycup.dataproviders.ServiceProviderDBTable;
+import com.pantrycup.entities.ServiceProviderUser;
 
 @RestController
 public class ServiceProviderAjaxService 
@@ -9,6 +15,9 @@ public class ServiceProviderAjaxService
 	@RequestMapping(value = "/search/serviceprovider")
 	public String getServiceProvidersViaAjax() 
 	{
-		return "welcome";
+		ServiceProviderDBTable serviceProviderTable = new ServiceProviderDBTable();
+		List<ServiceProviderUser> serviceProviderList = serviceProviderTable.fetchAllServiceProviders();
+		JSONObject serviceProviderJSON = new JSONObject(serviceProviderList);
+		return serviceProviderJSON.toString();
 	}
 }
