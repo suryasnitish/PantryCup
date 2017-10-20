1bd69dc9b7fdb10ed5897329efa52a3ab837a474
Surya Nitish <suryasnitish@gmail.com>
2017-10-20 08:42:05 +0530

    Session scoped variable

--- src/com/pantrycup/spring/beans/session/UserSession.java
+++ src/com/pantrycup/spring/beans/session/UserSession.java
@@ -2,11 +2,12 @@ package com.pantrycup.spring.beans.session;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 import com.pantrycup.entities.CustomerUser;
 
 @Component
-@Scope(value="session")
+@Scope(value="session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class UserSession
 {
 	private CustomerUser currentUser;
